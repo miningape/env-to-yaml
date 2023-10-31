e38d3cccab0a7de24362eac7889893345013eec3
Kyle Johnson <miningape@gmail.com>
2023-10-31 17:10:30 +0100

    chore: added env2yml bin entry in package.json

--- index.js
+++ index.js
@@ -1,57 +1,65 @@
-const commandLineArgs = require('command-line-args')
-const fs = require('fs').promises
+#!/usr/bin/env node
+
+const commandLineArgs = require("command-line-args");
+const fs = require("fs").promises;
 
 async function main() {
-	const options = commandLineArgs([
-        {
-            name: "env",
-            alias: "e",
-            type: String,
-            multiple: true,
-            defaultValue: [],
-        },
-        {
-            name: "yaml",
-            alias: "y",
-            type: String,
-        },
-        {
-            name: "out",
-            alias: "o",
-            type: String,
-            defaultValue: "out.yml"
+  const options = commandLineArgs([
+    {
+      name: "env",
+      alias: "e",
+      type: String,
+      multiple: true,
+      defaultValue: [],
+    },
+    {
+      name: "yaml",
+      alias: "y",
+      type: String,
+    },
+    {
+      name: "out",
+      alias: "o",
+      type: String,
+      defaultValue: "out.yml",
+    },
+  ]);
+
+  const envs = await Promise.all(
+    options.env.map(async (filename) => {
+      console.log(filename);
+      const contents = await fs.readFile(filename);
+      const file = contents.toString();
+      const env = {};
+      file.replace(/(\w+)=(.+)/g, function ($0, $1, $2) {
+        env[$1] = $2;
+      });
+      return env;
+    })
+  );
+  const env = envs.reduce((acc, cur) => ({ ...acc, ...cur }), {});
+
+  const ymlBuffer = await fs.readFile(options.yaml);
+  const yml = ymlBuffer
+    .toString()
+    .replace(/\$\{(\w+)(?::-(.*))?}/g, ($0, $1, $2) => {
+      let envValue = env[$1];
+
+      if (!envValue) {
+        if (!$2) {
+          console.log(
+            `Error interpreting ${$1} - Neither provided with a default value or covered by env - using default value`
+          );
+          return "";
         }
-    ]);
 
-    const envs = await Promise.all(options.env.map(async (filename) => {
-        console.log(filename)
-        const contents = await fs.readFile(filename);
-        const file = contents.toString();
-        const env = {};
-        file.replace(/(\w+)=(.+)/g, function($0, $1, $2) { env[$1] = $2 });
-        return env;
-    }));
-    const env = envs.reduce((acc, cur) => ({ ...acc, ...cur }), {});
+        envValue = $2;
+      }
 
-    const ymlBuffer = await fs.readFile(options.yaml);
-    const yml = ymlBuffer
-        .toString()
-        .replace(/\$\{(\w+)(?::-(.*))?}/g, ($0, $1, $2) => {
-            let envValue = env[$1];
+      return envValue;
+    });
 
-            if (!envValue) {
-                if (!$2) {
-                    console.log(`Error interpreting ${$1} - Neither provided with a default value or covered by env - using default value`);
-                    return ""
-                }
-
-                envValue = $2;
-            }
-
-            return envValue;
-        });
-
-    await fs.writeFile(options.out, yml);
+  await fs.writeFile(options.out, yml);
 }
 
-main().catch(e => console.error(e));
+main().catch((e) => console.error(e));
